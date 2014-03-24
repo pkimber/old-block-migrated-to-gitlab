beb1ee2808ce06822c801c44b6f04c006f9ebce5
Patrick Kimber <code@pkimber.net>
2014-03-24 14:02:03 +0000

    template tag for block content status

--- block/templatetags/block_tags.py
+++ block/templatetags/block_tags.py
@@ -15,3 +15,8 @@ def block_add(url):
 @register.inclusion_tag('block/_moderate.html')
 def block_moderate(generic_content, can_remove=True):
     return dict(c=generic_content, can_remove=can_remove)
+
+
+@register.inclusion_tag('block/_status.html')
+def block_status(generic_content):
+    return dict(c=generic_content)
