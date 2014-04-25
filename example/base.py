fb2a8a55294444ec4fec8e823a8623f5221b0e72
Patrick Kimber <code@pkimber.net>
2014-04-25 15:17:13 +0100

    timezone

--- example/base.py
+++ example/base.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
-
 from __future__ import unicode_literals
+
 """ Django settings """
 from django.core.urlresolvers import reverse_lazy
 
@@ -39,7 +39,7 @@ USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = False
+USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
