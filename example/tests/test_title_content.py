b2be4f77c1d8a78e2c42a408a2b9acfc6fd70140
Patrick Kimber <code@pkimber.net>
2014-10-12 20:38:55 +0100

    wip - django 1.7 - convert to use factories - flakes #660

--- example/tests/test_title_content.py
+++ example/tests/test_title_content.py
@@ -4,47 +4,17 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 from block.tests.helper import check_content
-from block.tests.model_maker import (
-    make_page,
-    make_page_section,
-    make_section,
-)
 from block.tests.factories import PageSectionFactory
-from block.tests.scenario import init_app_block
 from example.tests.factories import (
     TitleBlockFactory,
     TitleFactory,
 )
 from login.tests.factories import UserFactory
-from login.tests.scenario import (
-    default_scenario_login,
-    get_user_staff,
-)
 
 from example.models import Title
-from example.tests.model_maker import (
-    make_title,
-    make_title_block,
-)
 
 
 class TestTitle(TestCase):
-
-    def setUp(self):
-        init_app_block()
-        default_scenario_login()
-        home = make_page('Home', 'home', 0, 'test.html')
-        body = make_section(
-            'body',
-            'example',
-            'Title',
-            'example.title.create'
-        )
-        self.home_body = make_page_section(home, body)
-
-    def test_next_order(self):
-        #self.assertGreater(Content.objects.next_order(self.section), 3)
-        pass
 
     def test_content_methods(self):
         c = TitleFactory()
