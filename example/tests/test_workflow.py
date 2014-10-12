7f10d79c51fc478226d867622d3817081d619956
Patrick Kimber <code@pkimber.net>
2014-10-12 21:15:01 +0100

    wip - django 1.7 - convert to use factories #660

--- example/tests/test_workflow.py
+++ example/tests/test_workflow.py
@@ -9,12 +9,25 @@ from block.tests.model_maker import (
     make_page_section,
     make_section,
 )
+from block.tests.factories import (
+    PageFactory,
+    PageSectionFactory,
+    SectionFactory,
+)
 from block.tests.scenario import init_app_block
+from login.tests.factories import (
+    TEST_PASSWORD,
+    UserFactory,
+)
 from login.tests.scenario import (
     default_scenario_login,
     get_user_staff,
 )
 
+from example.tests.factories import (
+    TitleBlockFactory,
+    TitleFactory,
+)
 from example.tests.model_maker import (
     make_title,
     make_title_block,
@@ -24,22 +37,15 @@ from example.tests.model_maker import (
 class TestWorkflow(TestCase):
 
     def setUp(self):
-        init_app_block()
-        default_scenario_login()
-        home = make_page('Home', 'home', 0, 'test.html')
-        body = make_section(
-            'body',
-            'example',
-            'Title',
-            'example/home.html'
+        user = UserFactory(username='staff', is_staff=True)
+        self.assertTrue(
+            self.client.login(username=user.username, password=TEST_PASSWORD)
         )
-        home_body = make_page_section(home, body)
-        self.block = make_title_block(home_body)
-        self.content = make_title(
-            self.block,
-            1,
-            'Saints'
-        )
+        page = PageFactory()
+        section = SectionFactory()
+        page_section = PageSectionFactory(page=page, section=section)
+        self.block = TitleBlockFactory(page_section=page_section)
+        content = TitleFactory(block=self.block)
 
     def _get_pending(self):
         return self.block.content.get(
@@ -72,20 +78,20 @@ class TestWorkflow(TestCase):
         self.assertEqual(0, self._remove_count())
 
     def test_published(self):
-        self.block.publish(get_user_staff())
+        self.block.publish(UserFactory())
         self.assertEqual(1, self._pending_count())
         self.assertEqual(1, self._publish_count())
         self.assertEqual(0, self._remove_count())
 
     def test_remove_pending(self):
-        self.block.remove(get_user_staff())
+        self.block.remove(UserFactory())
         self.assertEqual(0, self._pending_count())
         self.assertEqual(0, self._publish_count())
         self.assertEqual(1, self._remove_count())
 
     def test_remove_published(self):
-        self.block.publish(get_user_staff())
-        self.block.remove(get_user_staff())
+        self.block.publish(UserFactory())
+        self.block.remove(UserFactory())
         self.assertEqual(0, self._pending_count())
         self.assertEqual(0, self._publish_count())
         self.assertEqual(1, self._remove_count())
