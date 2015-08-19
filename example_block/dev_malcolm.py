e875a169181930b08f1697a972762a463bd02ac6
Malcolm Dinsmore <m.dinsmore@talk21.com>
2015-08-19 09:37:12 +0100

    Settings file

---
+++ example_block/dev_malcolm.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'temp.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
