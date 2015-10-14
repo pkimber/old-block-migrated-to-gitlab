b6ab7599bfa46d5409e5bf71ff14b7374d576aca
Patrick Kimber <code@pkimber.net>
2015-10-14 16:48:01 +0100

    django - test - wizard - upload #1136

--- example_block/tests/test_view_image_wizard.py
+++ example_block/tests/test_view_image_wizard.py
@@ -44,7 +44,7 @@ def test_page_initial(client):
 
 
 @pytest.mark.django_db
-def test_page_initial_post_initial_to_image_form(client):
+def test_page_initial_to_image_form(client):
     """Choose to upload an image."""
     user = UserFactory(is_staff=True)
     assert client.login(username=user.username, password=TEST_PASSWORD) is True
@@ -61,7 +61,7 @@ def test_page_initial_post_initial_to_image_form(client):
 
 
 @pytest.mark.django_db
-def test_page_initial_post_initial_to_image_list(client):
+def test_page_initial_to_image_list(client):
     """Choose from a list of images."""
     user = UserFactory(is_staff=True)
     assert client.login(username=user.username, password=TEST_PASSWORD) is True
@@ -151,3 +151,75 @@ def test_image_remove_to_done(client):
     # check image is removed
     content.refresh_from_db()
     assert content.picture is None
+
+
+@pytest.mark.django_db
+def test_image_upload_to_done(client):
+    """Upload an image and attach to the content."""
+    #ImageFactory()
+    #image = ImageFactory()
+    user = UserFactory(is_staff=True)
+    assert client.login(username=user.username, password=TEST_PASSWORD) is True
+    session = client.session
+    # https://www.pkimber.net/howto/django/testing/wizard.html
+    session['wizard_image_wizard'] = {
+        'extra_data': {},
+        'step_data': {
+            'image_type': {
+                'image_type-image_type': ['i'],
+                'image_wizard-current_step': ['image_type'],
+                'csrfmiddlewaretoken': ['H1VQUMYxcPg9UTOH0lLmeYmw1SIhItIo']
+            },
+            'i': {
+                'i-title': ['TEst'],
+                'image_wizard-current_step': ['i'],
+                'csrfmiddlewaretoken': ['H1VQUMYxcPg9UTOH0lLmeYmw1SIhItIo']
+            }
+        },
+        'step': 'i',
+        'step_files': {
+            'image_type': {},
+            'i': {
+                'i-image': {
+                    'name': '80x80-place.png',
+                    'charset': None,
+                    'size': 260,
+                    'content_type': 'image/png',
+                    'tmp_name': '80x80-place.png',
+                }
+            }
+        }
+    }
+    session.save()
+    content = TitleFactory(picture=ImageFactory())
+    url = picture_url(content)
+    from django.core.files.uploadedfile import SimpleUploadedFile
+    import io
+    #from django.utils.image import Image
+    from PIL import Image #, ImageDraw
+    fp = io.BytesIO()
+    Image.new('P', (1,1)).save(fp, 'png')
+    fp.seek(0)
+
+    #img = Image.new('RGBA',(100, 100))
+
+    image = SimpleUploadedFile(
+        "file.png",
+        fp.read(),
+        #bytes("file_content", 'UTF-8'),
+        content_type="image/png",
+    )
+    data = {
+        'image_wizard-current_step': ImageTypeForm.FORM_IMAGE,
+        'image_type-image_type': ImageTypeForm.REMOVE,
+        'i-title': 'Title for My Image',
+        'i-image': image,
+    }
+    response = client.post(url, data)
+    assert 302 == response.status_code
+    # make sure we redirect to page design
+    page_design_url = content.block.page_section.page.get_design_url()
+    assert page_design_url in response['Location']
+    # check image is removed
+    content.refresh_from_db()
+    assert content.picture is not None
