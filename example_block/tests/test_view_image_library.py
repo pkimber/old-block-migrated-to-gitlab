00e5286d34bde91344a8c7120c47bc36de7dc607
Patrick Kimber <code@pkimber.net>
2015-10-18 20:36:16 +0100

    image wizard - choose category #1136

--- example_block/tests/test_view_image_library.py
+++ example_block/tests/test_view_image_library.py
@@ -20,18 +20,18 @@ from login.tests.factories import (
 )
 
 
-def reverse_url(content, url_name):
+def reverse_url(content, url_name, category=None):
     """Get the wizard url for a piece of content."""
     content_type = ContentType.objects.get_for_model(content)
-    return reverse(
-        url_name,
-        kwargs={
-            'content': content_type.pk,
-            'pk': content.pk,
-            'field': 'picture',
-            'type': Wizard.SINGLE,
-        }
-    )
+    kwargs = {
+        'content': content_type.pk,
+        'pk': content.pk,
+        'field': 'picture',
+        'type': Wizard.SINGLE,
+    }
+    if category:
+        kwargs.update(category=category.slug)
+    return reverse(url_name, kwargs=kwargs)
 
 
 @pytest.mark.django_db
@@ -43,6 +43,30 @@ def test_wizard_image_choose(client):
     assert content.picture is None
     assert client.login(username=user.username, password=TEST_PASSWORD) is True
     url = reverse_url(content, 'block.wizard.image.choose')
+    data = {
+        'images': image.pk,
+    }
+    response = client.post(url, data)
+    # check
+    assert 302 == response.status_code
+    expect = content.block.page_section.page.get_design_url()
+    assert expect in response['Location']
+    content.refresh_from_db()
+    assert image == content.picture
+
+
+@pytest.mark.django_db
+def test_wizard_image_choose_category(client):
+    """Choose from images in the selected category."""
+    content = TitleFactory()
+    category = ImageCategoryFactory()
+    ImageFactory()
+    image = ImageFactory(category=category)
+    user = UserFactory(is_staff=True)
+    assert content.picture is None
+    assert client.login(username=user.username, password=TEST_PASSWORD) is True
+    url = reverse_url(content, 'block.wizard.image.choose', category=category)
+    assert category.slug in url
     data = {
         'images': image.pk,
     }
--- example_block/tests/test_view_perm.py
+++ example_block/tests/test_view_perm.py
@@ -5,28 +5,37 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 
 from block.models import Wizard
+from block.tests.factories import ImageCategoryFactory
 from example_block.tests.factories import TitleFactory
 from login.tests.fixture import perm_check
 
 
-def reverse_url(content, url_name):
+def reverse_url(content, url_name, category=None):
     """Get the wizard url for a piece of content."""
     content_type = ContentType.objects.get_for_model(content)
-    return reverse(
-        url_name,
-        kwargs={
-            'content': content_type.pk,
-            'pk': content.pk,
-            'field': 'picture',
-            'type': Wizard.SINGLE,
-        }
-    )
+    kwargs = {
+        'content': content_type.pk,
+        'pk': content.pk,
+        'field': 'picture',
+        'type': Wizard.SINGLE,
+    }
+    if category:
+        kwargs.update(category=category.slug)
+    return reverse(url_name, kwargs=kwargs)
 
 
 @pytest.mark.django_db
 def test_wizard_image_choose(perm_check):
     content = TitleFactory()
     url = reverse_url(content, 'block.wizard.image.choose')
+    perm_check.staff(url)
+
+
+@pytest.mark.django_db
+def test_wizard_image_choose_category(perm_check):
+    category = ImageCategoryFactory()
+    content = TitleFactory()
+    url = reverse_url(content, 'block.wizard.image.choose', category=category)
     perm_check.staff(url)
 
 
