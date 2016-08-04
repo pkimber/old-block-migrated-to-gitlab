a48ac2ea917b080140c6a0983e058d9d8a29955a
Malcolm Dinsmore <zebyea@gmail.com>
2016-08-04 17:16:38 +0100

    version 0.1.54

--- setup.py
+++ setup.py
@@ -20,17 +20,17 @@ def get_readme():
 
 setup(
     name='kb-block',
-    packages=['block', 'block.urls', 'block.migrations', 'block.templatetags', 'block.management', 'block.management.commands', 'block.tests'],
+    packages=['block', 'block.management', 'block.management.commands', 'block.tests', 'block.migrations', 'block.urls', 'block.templatetags'],
     package_data={
         'block': [
             'templates/*.*',
             'templates/block/*.*',
         ],
     },
-    version='0.1.53',
+    version='0.1.54',
     description='block',
-    author='Patrick Kimber',
-    author_email='code@pkimber.net',
+    author='Malcolm Dinsmore',
+    author_email='zebyea@gmail.com',
     url='git@github.com:pkimber/block.git',
     classifiers=[
         'Development Status :: 1 - Planning',
