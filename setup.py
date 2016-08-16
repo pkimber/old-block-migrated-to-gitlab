0e61f70d30d84ee7ff106a5e8c6445037ff17f7f
Malcolm Dinsmore <zebyea@gmail.com>
2016-08-16 09:36:03 +0100

    version 0.1.55

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'templates/block/*.*',
         ],
     },
-    version='0.1.54',
+    version='0.1.55',
     description='block',
     author='Malcolm Dinsmore',
     author_email='zebyea@gmail.com',
