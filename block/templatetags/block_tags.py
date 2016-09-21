ae5c3ed61be38da7e14817b38b0e4ccf8224df9c
Tim Bushell <tcbushell@gmail.com>
2016-09-21 13:51:17 +0000

    Captioning fixed for null values on Moderate tags

--- block/templatetags/block_tags.py
+++ block/templatetags/block_tags.py
@@ -6,12 +6,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('block/_add.html')
-def block_add(url, caption=None, return_path=None):
+def block_add(url, caption="", return_path=None):
     return dict(url=url, caption=caption, return_path=return_path)
 
 
 @register.inclusion_tag('block/_moderate.html')
-def block_moderate(generic_content, can_remove=True, caption=None, return_path=None):
+def block_moderate(generic_content, can_remove=True, caption="", return_path=None):
     return dict(c=generic_content, can_remove=can_remove, caption=caption, return_path=return_path)
 
 
