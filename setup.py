46fd1f1a27399de9ccb3165558fd55e030e75b3b
Patrick Kimber <code@pkimber.net>
2016-10-02 17:37:07 +0100

    version 0.1.60

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'templates/block/*.*',
         ],
     },
-    version='0.1.59',
+    version='0.1.60',
     description='block',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
