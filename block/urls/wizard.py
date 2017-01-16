dd27771df63cba5e07f978b92ee3f4b9cba6d15e
Malcolm Dinsmore <zebyea@gmail.com>
2017-01-16 15:26:16 +0000

    Pep 8

--- block/urls/wizard.py
+++ block/urls/wizard.py
@@ -21,68 +21,84 @@ from block.views import (
 
 urlpatterns = [
     # image wizard
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/choose/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/choose/$',
         view=WizardImageChoose.as_view(),
         name='block.wizard.image.choose'
         ),
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/(?P<category>[-\w\d]+)/choose/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/(?P<category>[-\w\d]+)/choose/$',
         view=WizardImageChoose.as_view(),
         name='block.wizard.image.choose'
         ),
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/order/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/order/$',
         view=WizardImageOrder.as_view(),
         name='block.wizard.image.order'
         ),
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/option/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/option/$',
         view=WizardImageOption.as_view(),
         name='block.wizard.image.option'
         ),
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/remove/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/remove/$',
         view=WizardImageRemove.as_view(),
         name='block.wizard.image.remove'
         ),
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/select/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/select/$',
         view=WizardImageSelect.as_view(),
         name='block.wizard.image.select'
         ),
-    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/upload/$',
+    url(regex=r'^image/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/upload/$',
         view=WizardImageUpload.as_view(),
         name='block.wizard.image.upload'
         ),
     # link wizard
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/choose/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/choose/$',
         view=WizardLinkChoose.as_view(),
         name='block.wizard.link.choose'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/(?P<category>[-\w\d]+)/choose/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        '(?P<type>[-\w\d]+)/(?P<category>[-\w\d]+)/choose/$',
         view=WizardLinkChoose.as_view(),
         name='block.wizard.link.choose'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/external/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        '(?P<type>[-\w\d]+)/external/$',
         view=WizardLinkExternal.as_view(),
         name='block.wizard.link.external'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/option/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/option/$',
         view=WizardLinkOption.as_view(),
         name='block.wizard.link.option'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/order/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/order/$',
         view=WizardLinkOrder.as_view(),
         name='block.wizard.link.order'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/remove/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/remove/$',
         view=WizardLinkRemove.as_view(),
         name='block.wizard.link.remove'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/select/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/select/$',
         view=WizardLinkSelect.as_view(),
         name='block.wizard.link.select'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/upload/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/upload/$',
         view=WizardLinkUpload.as_view(),
         name='block.wizard.link.upload'
         ),
-    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/(?P<type>[-\w\d]+)/page/$',
+    url(regex=r'^link/(?P<content>\d+)/(?P<pk>\d+)/(?P<field>[-\w\d]+)/'
+        r'(?P<type>[-\w\d]+)/page/$',
         view=WizardLinkPage.as_view(),
         name='block.wizard.link.page'
         ),
