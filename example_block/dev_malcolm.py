54894bc0cfc6e3b3d8dbb45304accccb53383b3d
Malcolm Dinsmore <zebyea@gmail.com>
2017-01-20 11:09:34 +0000

    Update for debug toolbar

--- example_block/dev_malcolm.py
+++ example_block/dev_malcolm.py
@@ -12,3 +12,30 @@ DATABASES = {
         'PORT': '',
     }
 }
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS += (
+    # 'django.contrib.formtools',
+    'django_extensions',
+)
+
+#force the debug toolbar to be displayed
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
+
+
+import socket
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '127.0.1.1',
+    'localhost',
+    socket.gethostbyname(socket.gethostname())
+]
+
