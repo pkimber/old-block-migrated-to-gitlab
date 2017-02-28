a2379ca5f7ba96352af2a8996243fa5110636c08
Tim Bushell <tcbushell@gmail.com>
2017-02-28 16:59:23 +0200

    Task command

--- block/tasks.py
+++ block/tasks.py
@@ -8,7 +8,7 @@ from easy_thumbnails.files import generate_all_aliases
 @shared_task
 def thumbnail_image(image_pk):
     try:
-        img = Image.options.get(id=image_pk)
+        img = Image.objects.get(id=image_pk)
     except Image.DoesNotExist:
         img = None
     return obj
