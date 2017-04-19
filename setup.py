5ba544955c94957fb87a4b7b10319adc492c56ee
Malcolm Dinsmore <code-md@kbsoftware.co.uk>
2017-04-19 21:35:54 +0100

    version 0.1.80

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'templates/block/*.*',
         ],
     },
-    version='0.1.77',
+    version='0.1.80',
     description='block',
     author='Malcolm Dinsmore',
     author_email='code-md@kbsoftware.co.uk',
